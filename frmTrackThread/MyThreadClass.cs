b0467b2ebd19b5fdc8f711009426151a1e375071
System.out.print ("Runance") <rebeccaaguilar047@gmail.com>
2024-10-15 22:23:27 +0800

    Add project files.

---
+++ frmTrackThread/MyThreadClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace frmTrackThread
+{
+    public class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Thread currentThread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + currentThread.Name + " Process = " + i);
+                Thread.Sleep(500);
+            }
+        }
+
+
+        public static void Thread2()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Thread currentThread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + currentThread.Name + " Process = " + i);
+                Thread.Sleep(1500); 
+            }
+        }
+    }
+}
---
+++ frmTrackThread/frmTrackThread.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmTrackThread
+{
+    public partial class frmTrackThread : Form
+    {
+        public frmTrackThread()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                label1.Text = "Thread Starts...";
+                Refresh();
+
+                Thread threadA = new Thread(new ThreadStart(MyThreadClass.Thread1));
+                Thread threadB = new Thread(new ThreadStart(MyThreadClass.Thread2));
+                Thread threadC = new Thread(new ThreadStart(MyThreadClass.Thread2));
+                Thread threadD = new Thread(new ThreadStart(MyThreadClass.Thread1));
+
+                threadA.Priority = ThreadPriority.Highest;
+                threadB.Priority = ThreadPriority.Normal;
+                threadC.Priority = ThreadPriority.AboveNormal;
+                threadD.Priority = ThreadPriority.BelowNormal;
+
+                threadA.Name = "Thread A";
+                threadB.Name = "Thread B";
+                threadC.Name = "Thread C";
+                threadD.Name = "Thread D";
+
+
+                threadA.Start();
+                threadB.Start();
+                threadC.Start();
+                threadD.Start();
+
+                threadA.Join();
+                threadB.Join();
+                threadC.Join();
+                threadD.Join();
+
+                label1.Text = "End of Thread.";
+            }
+        }
+    }
+}
